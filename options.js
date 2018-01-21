318c905f11c50680c7685b8696a121fd8bb2ec93
Dan Trevino <dantrevino@gmail.com>
2018-01-21 00:55:15 -0800

    update select process. add coins.

--- options.js
+++ options.js
@@ -1,13 +1,15 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var currency = document.getElementById('currency').value;
+  var currency = document.getElementById('id_currency_multi').value;
+  var curr_sel = []
+  curr_sel.push(currency)
   // var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    currencies: currency
-  }, function() {
+  chrome.storage.sync.set({'currencies': curr_sel }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
+    var watch = document.getElementById('watch-list');
     status.textContent = 'Options saved.';
+    watch.textContent = curr_sel.toString()
     setTimeout(function() {
       status.textContent = '';
     }, 750);
@@ -18,13 +20,11 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    currency: 'Bitcoin'
-  }, function(items) {
+  chrome.storage.sync.get({'currencies': 'Bitcoin' }, function(items) {
     // document.getElementById('currency').value = items.currency;
-    document.getElementById('like').innerHTML = items.likesColor;
+    var watch = document.getElementById('watch-list').textContent=items.currencies;
+    // document.getElementById('like').innerHTML = items.likesColor;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
