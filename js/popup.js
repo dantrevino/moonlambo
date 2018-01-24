09ffaafb1b4a417f91bed4022cdfaaa959c2f836
Dan Trevino <dantrevino@gmail.com>
2018-01-23 23:58:10 -0800

    refactoring dynamic table render

--- js/popup.js
+++ js/popup.js
@@ -57,10 +57,11 @@ function add_new(id) {
 
 
 $(document).ready(function() {
+  restore_options()
 
   var coinselect = $('#coinselect')
 
-    $(coinselect).select2({
+  $(coinselect).select2({
       placeholder: 'Choose currencies to watch',
       allowClear: true,
       data: [{id:'BTC', text:'Bitcoin'},
@@ -165,72 +166,69 @@ $(document).ready(function() {
       {id:'XZC', text:'ZCoin'},
       {id:'ZEN', text:'ZenCash'}],
       width: '100%',
-      multiple: true,
-      // templateSelection: formatCoin
+      multiple: true
+      // templateSelection: formatCoin,
+      // templateResult: formatCoin
     });
 
-    $(coinselect).change(function(e) {
-      // var selections = ( JSON.stringify$(coinselect).select2('data')) );
-      var selections = $(coinselect).select2('data')
-      //console.log('Selected IDs: ' + ids);
-      console.log(selections)
-      var i = (selections.length) - 1
-      console.log('Selected options: ' + selections[i]['id'] );
-      // only selected options are sent
-      // if dynamically populating table, need to validate full
-      // list each time
-      // do not remove if removed from selected. leave that option
-      // to remove from table directly
-      // therefore only add if there is a new coin added to the end
-      // of the list
+  $(coinselect).change(function(e) {
+    // var selections = ( JSON.stringify$(coinselect).select2('data')) );
+    var selections = $(coinselect).select2('data')
+    //console.log('Selected IDs: ' + ids);
+    console.log(selections)
+    var i = (selections.length) - 1
+    console.log('Selected options: ' + selections[i]['id'] + ' - ' + selections[i]['text'] );
+    // only selected options are sent
+    // if dynamically populating table, need to validate full
+    // list each time
+    // do not remove if removed from selected. leave that option
+    // to remove from table directly
+    // therefore only add if there is a new coin added to the end
+    // of the list
 
-      // get coin (ids) from select2
-      // add new coin?
-          // if coin id already exists in table, then ignore
-          // else
-          // 1. query for price
-          // 2. add to table
-      // remove coin?
-          // remove coin from table
-            // query coin ids in table
-            // use convention to identify
-            // this should only happen if selection is open
-            // provide delete button in table
+    // get coin (ids) from select2
+    // add new coin?
+        // if coin id already exists in table, then ignore
+        // else
+        // 1. query for price
+        // 2. add to table
+    // remove coin?
+        // remove coin from table
+          // query coin ids in table
+          // use convention to identify
+          // this should only happen if selection is open
+          // provide delete button in table
 
-      var IDs = [];
-      $("#coinwatch").find(selections[i]['id']).each(function(){ IDs.push(this.id); });
+    var IDs = [];
+    $("#coinwatch").find(selections[i]['id']).each(function(){ IDs.push(this.id); });
 
-
-    });
+  });
 });
 
 function formatCoin (coin) {
-  if (!coin.id) {
-    return coin.text;
-  }
-  console.log('coin id found')
-  var $coin = $(
-    "<span class='cc " + coin.id + ">"  + coin.text + "</span>"
-  );
-  console.log(coin)
-  return $coin;
+  if (!coin.id) { return coin.text; }
+  var $coin = $('<span class="icon" style="box-sizing: inherit;"><i class="' + coin.element.value + '"> '  + coin.text + '</i></span>');  return $coin;
 };
 
-function loadTable () {
+function loadTable (inCoinList) {
+  if (inCoinList == null) {
+    inCoinList = [{mstCoinList:{"symbol":"BTC","price":"$0","cssClass":"has-text-info","changePct":"0.0"}}]
+  }
   var template = $('#charts').html()
-
   Mustache.parse(template)
   // build mstCoinList
-  // var rendered = Mustache.render(template, mstCoinList)
+  console.log('this is what were wriiting to coinlist')
+  console.log(inCoinList)
+  var rendered = Mustache.render(template, {mstCoinList:inCoinList})
 
   // testing object mstCoinList
-  var rendered = Mustache.render(template, {mstCoinList: [
-    {"symbol": "BTC","price": "$10,209","cssClass": "has-text-success","changePct": "3.2"},
-    {"symbol": "ETH","price": "$1,037","cssClass": "has-text-success","changePct": "7.3"},
-    {"symbol": "BCH","price": "$2,195","cssClass": "has-text-danger","changePct": "1.2"},
-    {"symbol": "LTC","price": "$198","cssClass": "has-text-success","changePct": "0.67"},
-    {"symbol": "XRP","price": "$1.32","cssClass": "has-text-success","changePct": "6.38"}
-  ]})
+  // var rendered = Mustache.render(template, {mstCoinList: [
+  //   {"symbol": "BTC","price": "$10,209","cssClass": "has-text-success","changePct": "3.2"},
+  //   {"symbol": "ETH","price": "$1,037","cssClass": "has-text-success","changePct": "7.3"},
+  //   {"symbol": "BCH","price": "$2,195","cssClass": "has-text-danger","changePct": "1.2"},
+  //   {"symbol": "LTC","price": "$198","cssClass": "has-text-success","changePct": "0.67"},
+  //   {"symbol": "XRP","price": "$1.32","cssClass": "has-text-success","changePct": "6.38"}
+  // ]})
 
   // turn off the spinner...
   $('#spinner').hide()
@@ -245,4 +243,47 @@ function do_chartMst() {
 
 function removeCoin(coin) {
   console.log('removeCoin called')
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get('currencies', function(items) {
+    if (!chrome.runtime.error) {
+      fetchPrices(items.currencies);
+    }
+  });
+}
+
+// takes in as tring or array of string
+function fetchPrices(symbols) {
+  if ( symbols == null ) {
+    symbols='ADA,EOS'
+  }
+//  const url = 'https://min-api.cryptocompare.com/data/pricemultifull?tsyms=USD&fsyms=' + items.currencies + '&extraParams=CryptoWatch'
+// const url = 'https://min-api.cryptocompare.com/data/pricemultifull?tsyms=USD&fsyms=BTC,BTC,CLOAK,ETH,BTS,BCH,BURST'
+  const url = 'https://min-api.cryptocompare.com/data/pricemultifull?tsyms=USD&fsyms=' + symbols + '&extraParams=CryptoWatch'
+  const PLUS = "has-text-success"
+  const MINUS = "has-text-danger"
+  var coinArray = []
+  axios.get(url).then(function(res){
+    for (coin in res.data.DISPLAY){
+      coinArray.push(res.data.DISPLAY[coin])
+    }
+    var renderArr = []
+    for( var i = 0; i< coinArray.length; i++ ){
+      // var chg = coinArray[i]['USD']['CHANGEPCT24HOUR']
+      var cssCls = coinArray[i]['USD']['CHANGEPCT24HOUR'] >= 0 ? PLUS : MINUS
+      var arrSymbol = coinArray[i]['USD']['FROMSYMBOL']
+      var arrPrice = coinArray[i]['USD']['PRICE']
+      var arrChgPct = coinArray[i]['USD']['CHANGEPCT24HOUR']
+      renderArr.push({"symbol":arrSymbol,"price":arrPrice,"cssClass": cssCls, "changePct": arrChgPct})
+    }
+    console.log('array coming out of fetchPrices')
+    console.log(renderArr)
+    console.log('---')
+    loadTable(renderArr)
+  }).catch(function(err){
+    console.log(err)
+  });
 }
